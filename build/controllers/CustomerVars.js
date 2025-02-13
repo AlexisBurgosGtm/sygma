11baecfcb4afe6e7885f340632518d8296d6c698
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2025-02-13 12:49:38 -0600

    TIPOS AGREGADOS

--- build/controllers/CustomerVars.js
+++ build/controllers/CustomerVars.js
@@ -24,7 +24,20 @@ let tipo_negocios = [
     { tipo: 'RESTAURANTE', descripcion: 'RESTAURANTE'},
     { tipo: 'ESCUELA', descripcion: 'ESCUELA'},
     { tipo: 'HOTEL', descripcion: 'HOTEL'},
-    { tipo: 'FARMACIA', descripcion: 'FARMACIA'}
+    { tipo: 'FARMACIA', descripcion: 'FARMACIA'},
+    { tipo: 'CERERIA', descripcion: 'CERERIA'},
+    { tipo: 'CLINICA', descripcion: 'CLINICA'},
+    { tipo: 'COMERCIAL', descripcion: 'COMERCIAL'},
+    { tipo: 'CORPORACION', descripcion: 'CORPORACION'},
+    { tipo: 'BOUTIQUE', descripcion: 'BOUTIQUE'},
+    { tipo: 'DISTRIBUIDORA', descripcion: 'DISTRIBUIDORA'},
+    { tipo: 'GASOLINERA', descripcion: 'GASOLINERA'},
+    { tipo: 'MINIDESPENSA', descripcion: 'MINI DESPENSA'},
+    { tipo: 'LIBRERIA', descripcion: 'LIBRERIA'},
+    { tipo: 'MISCELANEA', descripcion: 'MISCELANEA'},
+    { tipo: 'MUNDODE3', descripcion: 'MUNDO DE 3'},
+    { tipo: 'TORTILLERIA', descripcion: 'TORTILLERIA'},
+    { tipo: 'VARIEDADES', descripcion: 'VARIEDADES'}
 ]
 
 function get_tipo_precios(){
